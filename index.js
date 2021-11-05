36662bc1c5429159b183520513e68bb00017304a
Tania Tasmin <taniat.kaau@email.com>
2021-11-04 20:46:16 -0400

    Modify index.js to include fetchCoordsByIP function

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ fetchMyIP((error, ip) => {
 
   console.log('It worked! Returned IP:' , ip);
 
-  fetchCoordsByIP (ip, (error, coordinates) => {
+  fetchCoordsByIP(ip, (error, coordinates) => {
     if (error) {
       console.log("It didn't work!" , error);
       return;
