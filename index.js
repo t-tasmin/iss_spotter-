68565f07edbca791079fba732b43a1ec1ca47b99
Tania Tasmin <taniat.kaau@email.com>
2021-11-04 20:45:43 -0400

    Modify index.js to include fetchCoordsByIP

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-const { fetchMyIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -8,4 +8,15 @@ fetchMyIP((error, ip) => {
   }
 
   console.log('It worked! Returned IP:' , ip);
+
+  fetchCoordsByIP (ip, (error, coordinates) => {
+    if (error) {
+      console.log("It didn't work!" , error);
+      return;
+    }
+  
+    console.log('It worked! Returned Coordinates:' , coordinates);
+    
+  });
+
 });
