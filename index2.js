d87129f53a6a8738bbbcc3409f10fe2a7d821d1f
Tania Tasmin <taniat.kaau@email.com>
2021-11-05 07:01:40 -0400

    Replace callback hell with promises

---
+++ index2.js
@@ -0,0 +1,28 @@
+//const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes }= require('./iss_promised');
+/*
+fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => console.log(body));
+*/
+
+
+// index2.js
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+// see index.js for printPassTimes
+// copy it from there, or better yet, moduralize and require it in both files
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+// Call
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  });
