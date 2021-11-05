7525473b71b74a60f13fa52509124878f1c4ef47
Tania Tasmin <taniat.kaau@email.com>
2021-11-04 21:51:45 -0400

    Modify index.js to include nextISSTimesForMyLocation function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // index.js
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+/*
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes , nextISSTimesForMyLocation} = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -29,3 +30,37 @@ fetchMyIP((error, ip) => {
   });// End of fetchCoordsByIP
 
 });// End of fetchMyIP
+*/
+
+// index.js
+
+
+const { nextISSTimesForMyLocation } = require('./iss');
+
+/** 
+ * Input: 
+ *   Array of data objects defining the next fly-overs of the ISS.
+ *   [ { risetime: <number>, duration: <number> }, ... ]
+ * Returns: 
+ *   undefined
+ * Sideffect: 
+ *   Console log messages to make that data more human readable.
+ *   Example output:
+ *   Next pass at Mon Jun 10 2019 20:11:44 GMT-0700 (Pacific Daylight Time) for 468 seconds!
+ */
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+nextISSTimesForMyLocation((error, passTimes) => {
+  if (error) {
+    return console.log("It didn't work!", error);
+  }
+  // success, print out the deets!
+  printPassTimes(passTimes);
+});
